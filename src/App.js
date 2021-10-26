9c1108999088535d99db53ebcc3b139f810f27aa
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-10-26 21:44:17 +0100

    Create [GET]fetch request to get all users

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Switch, Route } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CreateRecipeForm from "./components/CreateRecipeForm";
 import Users from "./pages/Users";
 import User from "./pages/User";
@@ -8,6 +8,17 @@ import CreateUserForm from "./components/CreateUserForm";
 
 function App() {
   const [recipes, setRecipes] = useState([]);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3030/users")
+      .then((res) => res.json())
+      .then((usersData) => {
+        console.log({ usersData: usersData.data });
+        setUsers(usersData.data);
+      });
+  }, []);
+
 
   return (
     <>
