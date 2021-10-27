4d020b129002e25073e1c00137dff6d8a8d4545c
conor <conor.m@me.com>
2021-10-27 10:04:31 +0100

    create user form update

--- src/components/CreateUserForm.jsx
+++ src/components/CreateUserForm.jsx
@@ -1,105 +1,125 @@
 import { useState } from "react";
 
 export default function CreateUserForm({ users, setUsers }) {
-  const [userName, setUserName] = useState("");
-  const [email, setEmail] = useState("");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
+    const [userName, setUserName] = useState("");
+    const [email, setEmail] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
 
-  const handleUserName = (event) => {
-    event.preventDefault();
+    const handleUserName = (event) => {
+        event.preventDefault();
 
-    setUserName(event.target.vaule);
-  };
-
-  const handleEmail = (event) => {
-    event.preventDefault();
-
-    setEmail(event.target.vaule);
-  };
-
-  const handleFirstName = (event) => {
-    event.preventDefault();
-
-    setFirstName(event.target.vaule);
-  };
-
-  const handleLastName = (event) => {
-    event.preventDefault();
-
-    setLastName(event.target.vaule);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    const userToCreate = {
-      userName,
-      email,
-    };
-    console.log("User to create: ", userToCreate);
-
-    const fetchOptions = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(userToCreate),
+        setUserName(event.target.vaule);
     };
 
-    fetch("http://localhost:3030/users", fetchOptions)
-      .then((res) => res.json())
-      .then((newUser) => {
-        const profileToCreate = {
-          firstName,
-          lastName,
-          userId: newUser.id,
+    const handleEmail = (event) => {
+        event.preventDefault();
+
+        setEmail(event.target.vaule);
+    };
+
+    const handleFirstName = (event) => {
+        event.preventDefault();
+
+        setFirstName(event.target.vaule);
+    };
+
+    const handleLastName = (event) => {
+        event.preventDefault();
+
+        setLastName(event.target.vaule);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const userToCreate = {
+            userName,
+            email,
         };
-        const fetchTools = {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(profileToCreate),
+        console.log("User to create: ", userToCreate);
+
+        const fetchOptions = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(userToCreate),
         };
 
-        fetch("http://localhost:3030/users", fetchTools)
-          .then((res) => res.json())
-          .then((newProfile) => {
-            const userToAdd = {
-              ...newUser,
-              profile: newProfile,
-            };
+        fetch("http://localhost:3030/users", fetchOptions)
+            .then((res) => res.json())
+            .then((newUser) => {
+                const profileToCreate = {
+                    firstName,
+                    lastName,
+                    userId: newUser.id,
+                };
+                const fetchTools = {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(profileToCreate),
+                };
 
-            setUsers([...users, userToAdd]);
-          });
-      });
-  };
+                fetch("http://localhost:3030/users", fetchTools)
+                    .then((res) => res.json())
+                    .then((newProfile) => {
+                        const userToAdd = {
+                            ...newUser,
+                            profile: newProfile,
+                        };
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <h1>Create User</h1>
-      <ul>
-        <li>
-          <label>Username</label>
-          <input onChange={handleUserName} />
-        </li>
-        <li>
-          <label>Email</label>
-          <input onChange={handleEmail} />
-        </li>
-        <li>
-          <label>First Name</label>
-          <input onChange={handleFirstName} />
-        </li>
-        <li>
-          <label>Last Name</label>
-          <input onChange={handleLastName} />
-        </li>
-        <li>
-          <button type="submit">Submit</button>
-        </li>
-      </ul>
-    </form>
-  );
+                        setUsers([...users, userToAdd]);
+                    });
+            });
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h1>Create User</h1>
+            <ul>
+                <li>
+                    <label>Username</label>
+                    <input
+                        onChange={handleUserName}
+                        id="userName"
+                        name="userName"
+                        type="text"
+                        value={userName} />
+                </li>
+                <li>
+                    <label>Email</label>
+                    <input
+                        onChange={handleEmail}
+                        id="email"
+                        name="email"
+                        type="text"
+                        value={email} />
+                </li>
+                <li>
+                    <label>First Name</label>
+                    <input
+                        onChange={handleFirstName}
+                        id="firstName"
+                        name="firstName"
+                        type="text"
+                        value={firstName} />
+                </li>
+                <li>
+                    <label>Last Name</label>
+                    <input
+                        onChange={handleLastName}
+                        id="lastName"
+                        name="lastName"
+                        type="text"
+                        value={lastName} />
+                </li>
+                <li>
+                    <button type="submit">Submit</button>
+                </li>
+            </ul>
+        </form>
+    );
 }
