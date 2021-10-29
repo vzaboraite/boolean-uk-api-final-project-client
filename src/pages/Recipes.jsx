d1dddfadba6ad1b6fec86e1b230474bb104a9c25
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-10-29 12:34:30 +0100

    Remove buttons from recipes component

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -5,32 +5,25 @@ export default function Recipes({ recipes }) {
   const history = useHistory();
 
   return (
-    <ul>
-      <li>
-        <button onClick={() => history.push("/recipes/create")}>
-          Add recipe
-        </button>
-      </li>
-      <li>
-      </li>
-      {recipes.map((recipe, index) => {
-        console.log(recipe);
-        const { title, prepTime, cookingTime, description } = recipe;
+    <>
+      <h2>All recipes</h2>
+      <ul>
+        {recipes.map((recipe, index) => {
+          console.log(recipe);
+          const { title, prepTime, cookingTime, description } = recipe;
 
-        return (
-          <li key={index}>
-            <p>
-              <span>Title: {title}</span> <br />
-              <span>Preparation time: {prepTime}</span> <br />
-              <span>Cooking time: {cookingTime}</span> <br />
-              <span>Description: {description}</span>
-            </p>
-            <button onClick={() => history.push(`/recipes/${recipe.id}/edit`)}>
-              Edit recipe
-        </button>
-          </li>
-        );
-      })}
-    </ul>
+          return (
+            <li key={index}>
+              <p>
+                <span>Title: {title}</span> <br />
+                <span>Preparation time: {prepTime}</span> <br />
+                <span>Cooking time: {cookingTime}</span> <br />
+                <span>Description: {description}</span>
+              </p>
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 }
